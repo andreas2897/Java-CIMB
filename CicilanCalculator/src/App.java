f60d1794961ffed05f9de54a04109f5d68fa2b4f
Andreas Saputra <AS40095X@cimbniaga.co.id>
2020-05-29 15:56:32 +0700

    commit weekendtask

--- CicilanCalculator/src/App.java
+++ CicilanCalculator/src/App.java
@@ -1,62 +1,55 @@
 package CicilanCalculator.src;
 
-import java.text.NumberFormat;
-
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
     public static Scanner scanner = new Scanner(System.in);
-    private static final int BULAN_DALAM_TAHUN = 12;
-    private static final int PERCENT = 100;
+    static final int BULAN_DALAM_TAHUN = 12;
+    static final int PERCENT = 100;
 
     public static void main(String[] args) throws Exception {
+        // Harga Awal = P
+        // Durasi Cicilan = D (tahun)
+        // Bunga = B
+        // Bayaran per bulan = M
+
+        // M = [P + (P * B / 100)] / D * 12
         int hargaAwal;
         int durasiCicilan;
         double bunga;
 
         while (true) {
-            hargaAwal = (int) readInputNumber("Harga Awal : ");
-            if (hargaAwal >= 1_000_000 && hargaAwal <= 10_000_000) {
+            hargaAwal = (int) Console.readInputNumber("Harga Awal: ");
+
+            if (hargaAwal >= 1_000_000 && hargaAwal <= 100_000_000) {
                 break;
             }
             System.out.println("Harga minimal 1.000.000");
         }
 
         while (true) {
+            durasiCicilan = (int) Console.readInputNumber("Durasi Cicilan: ");
 
-            durasiCicilan = (int) readInputNumber("durasi cicilan : ");
             if (durasiCicilan >= 1) {
                 break;
             }
-            System.out.println("durasi minimal 1");
+            System.out.println("Tahun minimal 1");
         }
 
         while (true) {
+            bunga = (int) Console.readInputNumber("Bunga: ");
 
-            bunga = readInputNumber("bunga : ");
             if (bunga >= 0) {
                 break;
             }
             System.out.println("Bunga minimal 0");
         }
 
-        double bayaranPerBulan = (hargaAwal + (hargaAwal * (bunga / PERCENT))) / (durasiCicilan * BULAN_DALAM_TAHUN);
-        tampilanBayar(bayaranPerBulan);
+        double bayaranPerBulan = Hitung.hitungBayaranPerBulan(hargaAwal, durasiCicilan, bunga);
 
-        double sisaBayaran;
-        for (int i = 0; i < durasiCicilan * BULAN_DALAM_TAHUN; i++){
-            sisaBayaran -= bayaranPerBulan;
-            System.out.println(NumberFormat.getCurrencyInstance().format(sisaBayaran);
-        }
-    }
+        Show.tampilBayaran(bayaranPerBulan);
 
-    public static int readInputNumber(String message) {
-        System.out.print(message);
-        return scanner.nextDouble();
-    }
-
-    public static void tampilanBayar(double bayaranPerBulan) {
-        System.out.println("pembayaran setiap bulan");
-        System.out.println(NumberFormat.getCurrencyInstance().format(bayaranPerBulan));
+        Hitung.hitungSisaBayaranPerBulan(hargaAwal, durasiCicilan, bunga, bayaranPerBulan);
     }
 }
